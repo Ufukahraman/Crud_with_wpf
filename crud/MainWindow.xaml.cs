65bef1b7834d367fb502d2c32e6346fa89060b52
Ufuk Can KAHRAMAN <ufukcankahraman@outlook.com>
2023-09-01 15:30:59 +0300

    WPF de basit bir crud operasyonu örneği

---
+++ crud/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+
+namespace crud
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadGrid();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-TACD860;Initial Catalog=crud;Integrated Security=True");
+
+        public void clearData()
+        {
+            name_txt.Clear();
+            age_txt.Clear();
+            gender_txt.Clear();
+            city_txt.Clear();
+            search_txt.Clear();
+        }
+
+        public void LoadGrid()
+        {
+            SqlCommand cmd = new SqlCommand(" SELECT * FROM FirstTable",con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            datagrid.ItemsSource = dt.DefaultView;
+        }
+
+        public bool isValid()
+        {
+            if (name_txt.Text ==string.Empty)
+            {
+                MessageBox.Show("Name is required", "Failed",MessageBoxButton.OK,MessageBoxImage.Error);
+                return false;
+            }
+            if (age_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Age is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (gender_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Gender is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (city_txt.Text == string.Empty)
+            {
+                MessageBox.Show("City is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void btnclear_Click(object sender, RoutedEventArgs e)
+        {
+            clearData();  
+        }
+
+        private void btninsert_Click(object sender, RoutedEventArgs e)
+        {
+            if (isValid())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO FirstTable VALUES(@Name, @Age, @Gender, @City)", con);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@Name", name_txt.Text);
+                    cmd.Parameters.AddWithValue("@Age", age_txt.Text);
+                    cmd.Parameters.AddWithValue("@Gender", gender_txt.Text);
+                    cmd.Parameters.AddWithValue("@City", city_txt.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    LoadGrid();
+                    MessageBox.Show("Succesfully registered", "Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                    clearData();
+                }
+                catch(SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btndelete_Click(object sender, RoutedEventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM FİrstTable where ID = "+search_txt.Text+ " ",con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record has been deleted", "Deleted", MessageBoxButton.OK, MessageBoxImage.Information);
+                con.Close();
+                clearData();
+                LoadGrid();
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show("Not Deleted"+ ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnupdate_Click(object sender, RoutedEventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd= new SqlCommand("UPDATE FirstTable SET Name = '"+name_txt.Text+"' , Age = '"+ age_txt.Text+"', Gender = '" + gender_txt.Text + "' , City = '" + city_txt.Text + "' WHERE ID = '"+search_txt.Text+"' ",con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record has been updated succesfully", "Updated", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+                clearData();
+                LoadGrid();
+            }
+        }
+    }
+}
